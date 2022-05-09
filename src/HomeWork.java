c9e2c08e2f569f38ee1a3c3417dd38737a469792
Anna <anusya8@gmail.com>
2022-05-09 19:21:22 +0300

    final part1

--- src/HomeWork.java
+++ src/HomeWork.java
@@ -2,21 +2,21 @@ public class HomeWork {
     public static void main(String[] args) {
         //вывод на консоль
         boolean bool = true;
-        String simpleString = "Ctroka";
-        char ='#&%';
-        int num a = 010;
-        int num b = 0b111;
-        int num c = 9;
-        int num d = 0x669;
+        String simpleString = "Stroka";
+        char q ='#';
+        int a = 010;
+        int b = 0b111;
+        int c = 9;
+        int d = 0x669;
         float fl = 8.8f;
         double db = 8.8;
         System.out.println("boolean bool = " + bool);
         System.out.println("String simpleString = " + simpleString);
-        System.out.println("char = " + char);
-        System.out.println("int num a = " + a);
-        System.out.println("int num b = " + b);
-        System.out.println("int num c = " + c);
-        System.out.println("int num d = " + d);
+        System.out.println("char q = " + q);
+        System.out.println("int a = " + a);
+        System.out.println("int b = " + b);
+        System.out.println("int c = " + c);
+        System.out.println("int d = " + d);
     }
 
 }
